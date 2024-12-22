ba581a39dad31b4d770f34b1deed7e6e9d166549
Mohamed Karroumi <mkaroumi123@gmail.com>
2024-12-22 22:01:22 +0100

    B9-18 add functions to retrieve current user role and name in auth helper

--- app/helpers/auth.php
+++ app/helpers/auth.php
@@ -31,3 +31,34 @@ function require_guest()
         exit;
     }
 }
+
+function getCurrentUserRole() {
+    return $_SESSION['user']['role'] ?? null;
+}
+
+function getCurrentUserName() {
+    return $_SESSION['user']['name'] ?? null;
+}
+
+function check_role($required_roles_set) {
+    if (!is_logged_in()) {
+        return false;
+    }
+    $user_role = $_SESSION['user']['role'];
+
+    if (!in_array($user_role, $required_roles_set)) {
+        return false;
+    }
+    return true;
+}
+
+function check_authorization($roles) {
+    if (in_array('all', $roles)) {
+        return;
+    }
+    if (is_logged_in() && !check_role($roles)) {
+        http_response_code(401);
+        require 'app/views/errors/401.php';
+        die();
+    }
+}
